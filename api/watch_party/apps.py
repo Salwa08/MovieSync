b0b2be7e66f05aaa6dc399f009fd99d7529a3c66
Salwa <sssalwa.kh@gmail.com>
2025-04-28 12:54:45 +0100

    Fixed config for APIs

--- api/watch_party/apps.py
+++ api/watch_party/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WatchPartyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "watch_party"
+    name = "api.watch_party"
